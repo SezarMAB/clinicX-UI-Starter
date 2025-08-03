1b2b4b3cea87f622b61420482f2b3df9944e996c
Mahmoud Barakat <mahmoud.barakat@deutschebahn.com>
2025-08-03 16:23:01 +0200

    feat: add debug interceptor for logging HTTP requests and responses

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -5,3 +5,4 @@ export * from './token-interceptor';
 export * from './api-interceptor';
 export * from './error-interceptor';
 export * from './logging-interceptor';
+export * from './debug-interceptor';
