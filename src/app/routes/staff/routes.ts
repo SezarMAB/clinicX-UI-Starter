04885f53dc10338b57dd394963b05b1ae9441549
Mahmoud Barakat <mahmoud.barakat@deutschebahn.com>
2025-08-12 00:46:51 +0200

    refactor: simplify staff routing by removing individual routes in favor of dialog-based operations

--- src/app/routes/staff/routes.ts
+++ src/app/routes/staff/routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { staffResolver } from './shared/staff.resolver';
 
 export const routes: Routes = [
   {
@@ -7,21 +6,6 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./list/staff-list-page.component').then(m => m.StaffListPageComponent),
   },
-  {
-    path: 'new',
-    loadComponent: () =>
-      import('./form/staff-form-page.component').then(m => m.StaffFormPageComponent),
-  },
-  {
-    path: ':id',
-    loadComponent: () =>
-      import('./detail/staff-detail-page.component').then(m => m.StaffDetailPageComponent),
-    resolve: { staff: staffResolver },
-  },
-  {
-    path: ':id/edit',
-    loadComponent: () =>
-      import('./form/staff-form-page.component').then(m => m.StaffFormPageComponent),
-    resolve: { staff: staffResolver },
-  },
+  // All staff operations (view, create, edit) are now handled via dialogs
+  // opened from the staff list component
 ];
